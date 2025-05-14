713a9dca8a87e7f6568568d95e0a9ec0d28c9f5c
euclidescarlos <carloseuclidesneto@gmail.com>
2025-05-14 06:44:05 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/winewine/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.winewine
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configura padding das bordas do sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Botão que vai pra próxima tela
+        val botao = findViewById<Button>(R.id.button)
+        botao.setOnClickListener {
+            val intent = Intent(this, InicialActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
